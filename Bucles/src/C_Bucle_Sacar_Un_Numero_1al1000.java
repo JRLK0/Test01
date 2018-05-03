132b024c69a93ce3716dd051126ae36f6b2e5b3b
jorge <jorge@JMO-LaptopProX>
2018-05-03 12:20:48 +0200

    hola

---
+++ Bucles/src/C_Bucle_Sacar_Un_Numero_1al1000.java
@@ -0,0 +1,16 @@
+
+public class C_Bucle_Sacar_Un_Numero_1al1000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 0;
+		
+		
+		while (a<1000) {
+			a++;
+			System.out.println(a);
+		}
+	}
+
+}
